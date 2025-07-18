ae42d535c966109508d382589b936132d9ad27fd
DevilbobHellpants <trillizio3@hotmail.com>
2025-07-17 20:44:22 -0500

    Working Data Retrieval

--- Summer Game/Assets/NoahScripts/HighScoreTable.cs
+++ Summer Game/Assets/NoahScripts/HighScoreTable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.Networking;
 
 public class HighScoreTable : MonoBehaviour
 {
@@ -12,6 +12,9 @@ public class HighScoreTable : MonoBehaviour
     private List<HighscoreEntry> highscoreEntryList;
     private List<Transform> highscoreEntryTransformList;
 
+    string[] users;
+    string[] userInfo;
+
     private void Awake()
     {
         entryContainer = transform.Find("HighScoreEntryContainer");
@@ -19,24 +22,35 @@ public class HighScoreTable : MonoBehaviour
 
         entryTemplate.gameObject.SetActive(false);
 
-        highscoreEntryList = new List<HighscoreEntry>()
+        StartCoroutine(GetRequest("https://ntrillizio.com/Highscores.php"));
+
+        //GetHighScores();
+        /*highscoreEntryList = new List<HighscoreEntry>()
         {
-            new HighscoreEntry{ score = 521054, name = "AAA"},
-            new HighscoreEntry{ score = 123456, name = "ANT"},
-            new HighscoreEntry{ score = 473920, name = "CAT"},
-            new HighscoreEntry{ score = 927424, name = "NAN"},
-            new HighscoreEntry{ score = 375920, name = "PAT"},
-            new HighscoreEntry{ score = 103485, name = "ASS"},
-            new HighscoreEntry{ score = 749320, name = "JOE"}
+            new HighscoreEntry{ score = 54, name = "AAA"},
+            new HighscoreEntry{ score = 456, name = "ANT"},
+            new HighscoreEntry{ score = 120, name = "CAT"},
+            new HighscoreEntry{ score = 424, name = "NAN"},
+            new HighscoreEntry{ score = 720, name = "PAT"},
+            new HighscoreEntry{ score = 485, name = "ASS"},
+            new HighscoreEntry{ score = 320, name = "JOE"}
         };
 
         highscoreEntryTransformList = new List<Transform>();
         foreach (HighscoreEntry highscoreEntry in highscoreEntryList)
         {
             CreateHighScoreEntryTransform(highscoreEntry, entryContainer, highscoreEntryTransformList);
-        }
+        }*/
 
         //PlayerPrefs.Set
+    }
+
+    private void TranslateRawHighScores()
+    {
+        foreach (string user in users)
+        {
+            //highscoreEntryList.Add(new HighscoreEntry { user.Substring(user.IndexOf("*")), user.Substring(0) };
+        }
     }
 
     private void CreateHighScoreEntryTransform(HighscoreEntry highsccoreEntry, Transform container, List<Transform> transformList)
@@ -82,5 +96,66 @@ public class HighScoreTable : MonoBehaviour
     {
         public int score;
         public string name;
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get("https://ntrillizio.com/Highscores.php"))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError("Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.Log("Error: " + webRequest.downloadHandler.text);
+                    Debug.LogError("HTTP Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    Debug.Log("Received: " + webRequest.downloadHandler.text);
+
+                    string rawresponse = webRequest.downloadHandler.text;
+
+                    users = rawresponse.Split('*');
+
+                    highscoreEntryList = new List<HighscoreEntry>();
+                    
+                    foreach(string user in users)
+                    {
+                        if (user != "")
+                        {
+                            userInfo = user.Split(',');
+                            highscoreEntryList.Add(new HighscoreEntry { score = int.Parse(userInfo[1]), name = userInfo[0]});
+                        }
+                        //highscoreEntryList.Add(new HighscoreEntry { score = int.Parse(user.Substring(user.IndexOf(",") + 1)), name = user.Substring(0, user.IndexOf(",") - 1) });
+                    }
+
+                    for (int i = 0; i < highscoreEntryList.Count; i++)
+                    {
+                        for (int j = i + 1; j < highscoreEntryList.Count; j++)
+                        {
+                            if (highscoreEntryList[j].score > highscoreEntryList[i].score)
+                            {
+                                //swap
+                                HighscoreEntry tmp = highscoreEntryList[i];
+                                highscoreEntryList[i] = highscoreEntryList[j];
+                                highscoreEntryList[j] = tmp;
+                            }
+                        }
+                    }
+
+                    highscoreEntryTransformList = new List<Transform>();
+                    foreach (HighscoreEntry highscoreEntry in highscoreEntryList)
+                    {
+                        CreateHighScoreEntryTransform(highscoreEntry, entryContainer, highscoreEntryTransformList);
+                    }
+
+                    break;
+            }
+        }
     }
 }
